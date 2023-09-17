c9a904a7cff341aaec39ad79bab6ddebfceb94da
AlexSkir <a.skirnevskaia@gmail.com>
2023-09-17 03:28:18 +0300

    fix rerender side-block drawer

--- portfolio/src/js/PageLayout/SideBlock.js
+++ portfolio/src/js/PageLayout/SideBlock.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import MuiDrawer from '@mui/material/Drawer';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import LoadingBlock from '../Suspense/LoadingBlock';
 
 import Avatar from './Avatar';
-import SocialIconsBlock from './SocialIconsBlock';
-import ContactBlock from './ContactBlock';
 import avatarBase from '../../assets/images/avatar/avatar-base.png';
 import avatarHeart from '../../assets/images/avatar/avatar-heart.png';
 import avatarOk from '../../assets/images/avatar/avatar-ok.png';
 import avatarHi from '../../assets/images/avatar/avatar-hi.png';
 import avatar4042 from '../../assets/images/avatar/avatar-404.2.png';
+
+// const Avatar = lazy(() => import('./Avatar'));
+const SocialIconsBlock = lazy(() => import('./SocialIconsBlock'));
+const ContactBlock = lazy(() => import('./ContactBlock'));
+const MuiDrawer = lazy(() => import('@mui/material/Drawer'));
 
 const openedMixin = (theme) => ({
   position: 'relative',
@@ -112,48 +115,68 @@ export default function SideBlock(props) {
 
   return (
     <Box className="side-block-section" sx={{ display: { xs: 'none', md: 'block' } }}>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader
-          sx={{
-            position: 'absolute',
-            top: { xs: '10px', lg: '30px' },
-            right: 0,
-          }}
-        >
-          {open ? (
-            <Tooltip title={t('sideblock.closeBtnTooltip')}>
-              <IconButton onClick={handleDrawerClose}>
-                <ChevronLeftIcon sx={{ color: 'secondary.dark', width: '40px', height: '40px' }} />
-              </IconButton>
-            </Tooltip>
-          ) : (
-            <Tooltip title={t('sideblock.openBtnTooltip')}>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-              >
-                <ChevronRightIcon sx={{ color: 'secondary.main', width: '40px', height: '40px' }} />
-              </IconButton>
-            </Tooltip>
-          )}
-        </DrawerHeader>
-        <Avatar img={avatarImg} open={open} />
-        <Typography
-          variant="h4"
-          component="h4"
-          align="center"
-          sx={{ m: '15px 0', display: open ? 'block' : 'none' }}
-        >
-          {t('sideblock.name')}
-        </Typography>
-        <Typography variant="body2" sx={{ opacity: '0.5', display: open ? 'block' : 'none' }}>
-          {t('sideblock.position')}
-        </Typography>
-        <SocialIconsBlock open={open} />
-        <ContactBlock open={open} />
-      </Drawer>
+      <Suspense
+        fallback={
+          <LoadingBlock width={open ? '400px' : '70px'} height="100%" variant="rectangular" />
+        }
+      >
+        <Drawer variant="permanent" open={open}>
+          <Suspense fallback={<LoadingBlock width="100%" height="74px" variant="rectangular" />}>
+            <DrawerHeader
+              sx={{
+                position: 'absolute',
+                top: { xs: '10px', lg: '30px' },
+                right: 0,
+              }}
+            >
+              {open ? (
+                <Tooltip title={t('sideblock.closeBtnTooltip')}>
+                  <IconButton onClick={handleDrawerClose}>
+                    <ChevronLeftIcon
+                      sx={{ color: 'secondary.dark', width: '40px', height: '40px' }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              ) : (
+                <Tooltip title={t('sideblock.openBtnTooltip')}>
+                  <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                  >
+                    <ChevronRightIcon
+                      sx={{ color: 'secondary.main', width: '40px', height: '40px' }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              )}
+            </DrawerHeader>
+          </Suspense>
+
+          <Avatar img={avatarImg} open={open} />
+
+          <Typography
+            variant="h4"
+            component="h4"
+            align="center"
+            sx={{ m: '15px 0', display: open ? 'block' : 'none' }}
+          >
+            {t('sideblock.name')}
+          </Typography>
+          <Typography variant="body2" sx={{ opacity: '0.5', display: open ? 'block' : 'none' }}>
+            {t('sideblock.position')}
+          </Typography>
+
+          <Suspense fallback={<LoadingBlock width="100%" height="50px" variant="rounded" />}>
+            <SocialIconsBlock open={open} />
+          </Suspense>
+
+          <Suspense fallback={<LoadingBlock width="350px" height="350px" variant="rounded" />}>
+            <ContactBlock open={open} />
+          </Suspense>
+        </Drawer>
+      </Suspense>
     </Box>
   );
 }
