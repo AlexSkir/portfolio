da1e378b729db3f70631ed20ad80827794581c84
AlexSkir <a.skirnevskaia@gmail.com>
2023-10-04 23:02:06 +0300

    fix lazy for resume

--- portfolio/app/[lng]/resume/page.js
+++ portfolio/app/[lng]/resume/page.js
@@ -1,15 +1,18 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import getDictionary from '../../i18n/dictionaries';
-import Resume from './Resume';
+import LoadingBlock from '../components/common/LoadingBlock';
 
 const PageLayout = lazy(() => import('../components/PageLayout'));
+const Resume = lazy(() => import('./Resume'));
 
 export default async function Page({ params: { lng } }) {
   const tResume = await getDictionary(lng, 'resume');
   return (
     <PageLayout avatar="base" title={tResume.title} lng={lng}>
-      <Resume t={tResume} lng={lng} />
+      <Suspense fallback={<LoadingBlock width="100%" height="300px" variant="rectangular" />}>
+        <Resume t={tResume} lng={lng} />
+      </Suspense>
     </PageLayout>
   );
 }
