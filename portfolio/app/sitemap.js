e71e2a0670afdbcb02884ffeb670dfa61cf7c72f
AlexSkir <a.skirnevskaia@gmail.com>
2023-10-15 02:49:50 +0300

    fix sitemap

--- portfolio/app/sitemap.js
+++ portfolio/app/sitemap.js
@@ -7,28 +7,29 @@ const URL = 'https://alexskir.ru';
 export default async function sitemap() {
   const enProjects = Object.keys(projectArr(en)).map((project) => ({
     url: `${URL}/en/portfolio/${project}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
   }));
   const ruProjects = Object.keys(projectArr(ru)).map((project) => ({
     url: `${URL}/ru/portfolio/${project}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.5,
   }));
 
   const enRoutes = ['', 'portfolio', 'resume', 'contact'].map((route) => ({
     url: `${URL}/en/${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
   }));
   const ruRoutes = ['', 'portfolio', 'resume', 'contact'].map((route) => ({
     url: `${URL}/ru/${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 1,
   }));
 
   return [...enRoutes, ...ruRoutes, ...enProjects, ...ruProjects];
 }
-
-/* export default function sitemap() {
-  return [
-    {
-      url: 'https://alexskir.ru/',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-  ];
-} */
