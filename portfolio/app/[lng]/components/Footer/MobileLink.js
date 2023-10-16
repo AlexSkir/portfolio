e10510573422c0442fb14a51aff713a6342c2721
AlexSkir <a.skirnevskaia@gmail.com>
2023-10-17 01:27:09 +0300

    fix redirect to mobile links

--- portfolio/app/[lng]/components/Footer/MobileLink.js
+++ portfolio/app/[lng]/components/Footer/MobileLink.js
@@ -3,12 +3,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function MobileLink(props) {
   const { children } = props;
-  const { protocol, host, pathname, href } = window.location;
+  const { protocol, host } = window.location;
+  const pathname = usePathname();
 
-  const mobUrl = host.indexOf('m.') === -1 ? `${protocol}//m.${host}${pathname}` : href;
+  const mobUrl =
+    host.indexOf('m.') === -1
+      ? `${protocol}//m.${host}${pathname}`
+      : `${protocol}//${host}${pathname}`;
 
   return <Link href={mobUrl}>{children}</Link>;
 }
